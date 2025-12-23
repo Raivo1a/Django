2b94c4cf3524a1d980b0fcaacfcf4a845d43bb54
Christine Ekke <k.ekke2906@gmail.com>
2025-12-23 15:35:23 +0300

    hw 29

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.forms import ProductForm, ProductModeratorForm
-from catalog.models import Product
+from catalog.models import Product, Category
+from catalog.services import get_products_from_cache, get_products_by_category
 
 
 class ProductsListView(ListView):
     model = Product
 
     def get_queryset(self):
-        return Product.objects.filter(is_published=True).order_by('name')
+        products = get_products_from_cache()
+        return products.filter(is_published=True).order_by('name')
 
 
 class ProductDetailView(LoginRequiredMixin, DetailView):
@@ -63,3 +66,29 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
         if product.owner != request.user and not request.user.has_perm("can_delete_product"):
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
+
+
+class ProductsByCategoryView(ListView):
+    model = Product
+    template_name = 'catalog/products_by_category.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('pk')
+        self.category = get_object_or_404(Category, pk=category_id)
+        return Product.objects.filter(
+            category=self.category,
+            is_published=True
+        ).order_by('name')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_category'] = self.category
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'catalog/category_list.html'
+    context_object_name = 'categories'
